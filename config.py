12f36514043ffa2e8e67bbeee728ae8d48f2d8d9
kabu1 <kkabubenson@gmail.com>
2020-09-25 13:32:38 -0400

    set up database

---
+++ config.py
@@ -0,0 +1,28 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    QUOTES_API_KEY = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://k:qwerty1234@localhost/blog'
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
