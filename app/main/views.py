8a73fbc7fe87b7c0ef995742c4ce21e9518bd1b6
kabu1 <kkabubenson@gmail.com>
2020-09-25 10:28:39 -0400

    edit views.py

--- app/main/views.py
+++ app/main/views.py
@@ -8,4 +8,12 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    quote = get_quote()
+    pitches=Pitches.query.all()
+    identification = Pitches.user_id
+    posted_by = User.query.filter_by(id=identification).first()
+    user = User.query.filter_by(id=current_user.get_id()).first()
+
+    recent_post = Pitches.query.order_by(desc(Pitches.id)).all()
+
+    return render_template('pitches.html', quote=quote, pitches=pitches, posted_by=posted_by, user=user, recent_post=recent_post)
