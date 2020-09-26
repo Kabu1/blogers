207f286a80b51f83a133c6e57771a5188b1a2686
kabu1 <kkabubenson@gmail.com>
2020-09-26 16:12:34 -0400

    add sign up form

--- app/models.py
+++ app/models.py
@@ -1,18 +1,32 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     password_encrypt=db.Column(db.String(128))
+    @property   #write-only
+    def password(self):
+        raise AttributeError('You can only read this attribute')
 
+    @password.setter
+    def password(self, password):
+        self.password_encrypt = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_encrypt, password)
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'User{self.username}'
 class Role(db.Model):
     __tablename__ = 'roles'
 
