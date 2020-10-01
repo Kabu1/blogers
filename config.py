da31a779c6cc2f676f08491ba2b147a0ed55b437
kabu1 <kkabubenson@gmail.com>
2020-10-01 08:55:31 -0400

    deployment to heroku

--- config.py
+++ config.py
@@ -6,15 +6,13 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SECRET_KEY ='qwerty1234'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://k:qwerty1234@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
      #email config
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("flaskemailmoringa@gmail.com")
-    MAIL_PASSWORD = os.environ.get("qazplm098")
-
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
